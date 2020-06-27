3a20046a4c2f4d72127d30024386d44f615b5e38
jimmychen10 <jimmychen1996@yahoo.com>
2020-06-27 00:35:59 -0700

    Adding codes to repo

---
+++ react-excercise-1/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+    <div>
+      <h1>Favorite Foods</h1>
+      <ol>
+        <li>Ramen</li>
+        <li>Sushi</li>
+        <li>Tamago</li>
+      </ol>
+
+    </div>
+  , document.getElementById('root')
+);
